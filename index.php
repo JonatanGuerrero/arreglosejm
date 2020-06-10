24a90ec13d3da26456736037d907f534c7e864a1
JonatanGuerrero <66330879+JonatanGuerrero@users.noreply.github.com>
2020-06-10 18:01:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,41 @@ echo("<br><br>");
 echo($arreglo[0]);
 echo("<br><br>");
 echo($arreglo2["instrumento6"]);
+echo("<br><br>");
+echo("-----------------------------------------");
+echo("<br><br>");
+// foreach arreglos indexados
 
-// foreach y clases POO
+foreach($arreglo as $elemento){
+    echo($elemento);
+    echo("<br><br>");
 
-?>
+}
+
+echo("<br><br>");
+echo("-----------------------------------------");
+echo("<br><br>");
+// foreach arreglos asociativos
+
+$instrumentosBanda=array("Guitarra electrica","guitarra acustica","bajo","Piano","violin","Flauta");
+print_r($instrumentosBanda);
+echo("<br>");
+foreach($instrumentosBanda as $instrumento){
+echo($instrumento);
+echo("<br><br>");
+}
+echo("<br><br>");
+echo("-----------------------------------------");
+echo("<br><br>");
+//matrices
+
+$tienda=array(
+"producto1"=>array("papa",1500,3),
+"producto2"=>array("pastas",1500,3),
+"producto3"=>array("maiz",3000,1),
+
+);
+print_r($tienda);
+
+
+?> 
